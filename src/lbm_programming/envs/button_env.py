555ef986d13cc1b63d04adb49109d749396c648f
Tom Silver <tomssilver@gmail.com>
2025-10-27 13:17:29 -0400

    wip

--- src/lbm_programming/envs/button_env.py
+++ src/lbm_programming/envs/button_env.py
@@ -1,5 +1,7 @@
 """Simple 2D grid environment with pressable buttons."""
 
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import Any, SupportsFloat, TypeVar
 
@@ -33,9 +35,32 @@ class ButtonState:
         assert set(self.button_positions) == set(self.button_statuses)
         assert self.target_button in self.button_positions
 
+    def copy_with(self, robot_position: tuple[int, int]) -> ButtonState:
+        """Create a new ButtonState with the input values changed."""
+        return ButtonState(
+            robot_position=robot_position,
+            button_positions=self.button_positions,
+            button_statuses=self.button_statuses,
+            target_button=self.target_button,
+            human_response=self.human_response,
+            world_dims=self.world_dims,
+        )
+
 
 class ButtonAction:
     """An action in the button environment."""
+
+
+@dataclass(frozen=True)
+class MoveButtonAction(ButtonAction):
+    """Move the robot by some limited amount."""
+
+    dr: int  # should be -1, 0, 1
+    dc: int  # should be -1, 0, 1
+
+    def __post_init__(self) -> None:
+        assert -1 <= self.dr <= 1
+        assert -1 <= self.dc <= 1
 
 
 class ButtonEnv(gymnasium.Env[ButtonState, ButtonAction]):
@@ -49,9 +74,33 @@ class ButtonEnv(gymnasium.Env[ButtonState, ButtonAction]):
     def step(
         self, action: ButtonAction
     ) -> tuple[ButtonState, SupportsFloat, bool, bool, dict[str, Any]]:
-        import ipdb
+        assert self._current_state is not None, "Must call reset() first"
 
-        ipdb.set_trace()
+        # Handling moving the robot.
+        if isinstance(action, MoveButtonAction):
+            new_robot_position = (
+                self._current_state.robot_position[0] + action.dr,
+                self._current_state.robot_position[1] + action.dc,
+            )
+            # If the new robot position is out of bounds, stay in place.
+            if not (
+                0 <= new_robot_position[0] < self._current_state.world_dims[0]
+                and 0 <= new_robot_position[1] < self._current_state.world_dims[1]
+            ):
+                new_robot_position = self._current_state.robot_position
+            self._current_state = self._current_state.copy_with(
+                robot_position=new_robot_position
+            )
+
+        else:
+            raise NotImplementedError
+
+        # Coming later.
+        reward = 0.0
+        terminated = False
+        truncated = False
+
+        return self._get_obs(), reward, terminated, truncated, {}
 
     def reset(
         self,
@@ -83,7 +132,7 @@ class ButtonEnv(gymnasium.Env[ButtonState, ButtonAction]):
         for button_pos in self._current_state.button_positions.values():
             avatar_grid[button_pos] = "button"
         # Finish the image.
-        return render_avatar_grid(avatar_grid)   # type: ignore
+        return render_avatar_grid(avatar_grid)  # type: ignore
 
     def _get_obs(self) -> ButtonState:
         assert self._current_state is not None
--- tests/envs/test_button_env.py
+++ tests/envs/test_button_env.py
@@ -1,6 +1,6 @@
 """Tests for button_env.py."""
 
-from lbm_programming.envs.button_env import ButtonAction, ButtonEnv, ButtonState
+from lbm_programming.envs.button_env import ButtonEnv, ButtonState, MoveButtonAction
 
 
 def test_button_env():
@@ -8,11 +8,14 @@ def test_button_env():
     env = ButtonEnv()
     obs, _ = env.reset(seed=123)
     assert isinstance(obs, ButtonState)
-    img = env.render()
-    # import ipdb; ipdb.set_trace()
+
+    # Test moving.
+    action = MoveButtonAction(1, 0)
+    next_obs, _, _, _, _ = env.step(action)
+    assert obs.robot_position[0] + 1 == next_obs.robot_position[0]
+    assert obs.robot_position[1] == next_obs.robot_position[1]
 
     # Uncomment to save the image.
+    # img = env.render()
     # import imageio.v2 as iio
     # iio.imsave("button_test_image.png", img)
-
-    # TODO: finish this test
