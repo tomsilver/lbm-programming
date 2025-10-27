a3251ce67c6413d39fe9d660b5806b4397e349ca
Tom Silver <tomssilver@gmail.com>
2025-10-27 12:43:53 -0400

    demonstrate checks

--- src/lbm_programming/utils.py
+++ src/lbm_programming/utils.py
@@ -7,4 +7,5 @@ from lbm_programming.structs import Dog
 
 def get_good_dogs_of_breed(dogs: Set[Dog], breed: str) -> Set:
     """Get all good dogs of the specified breed."""
+    assert False
     return {d for d in dogs if d.is_good() and d.breed == breed}
