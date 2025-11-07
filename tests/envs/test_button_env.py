05976751da9afc173574e6bb719d42681e75b806
AZ <az2440@princeton.edu>
2025-11-07 09:43:13 -0500

    add actions + tests for actions

--- tests/envs/test_button_env.py
+++ tests/envs/test_button_env.py
@@ -1,5 +1,7 @@
 """Tests for button_env.py."""
 
+import numpy as np
+
 from lbm_programming.envs.button_env import ButtonEnv, ButtonState, MoveButtonAction
 
 
@@ -14,8 +16,32 @@ def test_button_env():
     next_obs, _, _, _, _ = env.step(action)
     assert obs.robot_position[0] + 1 == next_obs.robot_position[0]
     assert obs.robot_position[1] == next_obs.robot_position[1]
-
+    print(obs.robot_position[1])
     # Uncomment to save the image.
-    # img = env.render()
-    # import imageio.v2 as iio
-    # iio.imsave("button_test_image.png", img)
+    img = env.render()
+    import imageio.v2 as iio
+
+    iio.imsave("button_test_image.png", img)
+
+
+def test_random_walk():
+    """Tests for ButtonEnv()."""
+    env = ButtonEnv()
+    obs, _ = env.reset(seed=123)
+    assert isinstance(obs, ButtonState)
+    rng = np.random.default_rng()
+    action = env._sample_action(rng)
+    next_obs, _, _, _, _ = env.step(action)
+    # assert obs.robot_position[0] + 1 == next_obs.robot_position[0]
+    # assert obs.robot_position[1] == next_obs.robot_position[1]
+    print(next_obs.robot_position[1])
+    # Uncomment to save the image.
+    img = env.render()
+    import imageio.v2 as iio
+
+    iio.imsave("button_test_image.png", img)
+
+
+if __name__ == "__main__":
+    test_button_env()
+    test_random_walk()
