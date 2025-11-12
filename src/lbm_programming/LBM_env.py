b308edae17ed4ce8e2631b4697b85175213fee16
AZ <az2440@princeton.edu>
2025-11-12 14:51:03 -0500

    save work

--- src/lbm_programming/LBM_env.py
+++ src/lbm_programming/LBM_env.py
@@ -1,28 +1,15 @@
-"""Simple 2D grid environment with pressable buttons."""
-
-from __future__ import annotations
-
-from dataclasses import dataclass
-from typing import Any, SupportsFloat, TypeVar
-
+from src.lbm_programming.envs.button_env import ButtonEnv
 import gymnasium
 import numpy as np
 from prpl_utils.spaces import FunctionalSpace
 
-from lbm_programming.utils import render_avatar_grid
-
-RenderFrame = TypeVar("RenderFrame")
-
-
 @dataclass(frozen=True)
-class ButtonState:
+class LBMState:
     """The state in a button environment."""
 
     robot_position: tuple[int, int]  # row, col
     button_positions: dict[str, tuple[int, int]]  # name to button positions
     button_statuses: dict[str, bool]  # name of button to status
-    button_values: dict[str, float]  # value of button to succeed in pressing
-    button_good_bad: dict[str, bool]  # whther an LBM is "good" or "bad" 
     target_button: str  # name of the button that we want to press
     human_response: float | None  # used for querying the human
     world_dims: tuple[int, int]  # height and width
@@ -38,102 +25,69 @@ class ButtonState:
         assert set(self.button_positions) == set(self.button_statuses)
         assert self.target_button in self.button_positions
 
-    def copy_with(self, robot_position: tuple[int, int]) -> ButtonState:
+    def copy_with(self, robot_position: tuple[int, int]) -> LBMState:
         """Create a new ButtonState with the input values changed."""
-        return ButtonState(
+        return LBMState(
             robot_position=robot_position,
             button_positions=self.button_positions,
             button_statuses=self.button_statuses,
-            button_values=self.button_values,
             target_button=self.target_button,
             human_response=self.human_response,
             world_dims=self.world_dims,
         )
 
 
-class ButtonAction:
-    """An action in the button environment."""
+class LBMPrimitive:
+    """A primitive LBM."""
 
 
 @dataclass(frozen=True)
-class PressButtonAction(ButtonAction):
+class QueryLBM(LBMPrimitive):
     """Press the button in the button environment."""
 
-    val: float
 
-    def __post_init__(self) -> None:
-        assert -100 <= self.val <= 100
+
 
 @dataclass(frozen=True)
-class QueryLBMAction(ButtonAction):
+class QueryHuman(LBMPrimitive):
     """Press the button in the button environment."""
 
-    val: float
-
-    def __post_init__(self) -> None:
-        assert -100 <= self.val <= 100
 
 @dataclass(frozen=True)
-class QueryHumanAction(ButtonAction):
+class GetMotionPlan(LBMPrimitive):
     """Press the button in the button environment."""
-
-    val: float
-
-    def __post_init__(self) -> None:
-        assert -100 <= self.val <= 100
-
-
-@dataclass(frozen=True)
-class QueryHumanButtonAction(ButtonAction):
-    """Press the button in the button environment."""
-
-    def __post_init__(self) -> None:
-        return None
-
-
-@dataclass(frozen=True)
-class MoveButtonAction(ButtonAction):
-    """Move the robot by some limited amount."""
 
     dr: int  # should be -1, 0, 1
     dc: int  # should be -1, 0, 1
 
-    def __post_init__(self) -> None:
-        assert -1 <= self.dr <= 1
-        assert -1 <= self.dc <= 1
+def QueryLBM(value)
 
-@dataclass(frozen=True)
-class GetMotionPlanAction(ButtonAction):
-    """Move the robot by some limited amount."""
+class LBMEnv:
 
-    dr: int  # should be -1, 0, 1
-    dc: int  # should be -1, 0, 1
+    robot_position: tuple[int, int]  # row, col
+    button_positions: dict[str, tuple[int, int]]  # name to button positions
+    button_statuses: dict[str, bool]  # name of button to status
+    target_button: str  # name of the button that we want to press
+    world_dims: tuple[int, int]  # height and width
 
-    #def __post_init__(self) -> None:
-       # assert 0 <= self.dr <= 1
-      #  assert 0 <= self.dc <= 1
+    def __init__(self, buttonenv) -> None:
+        self.buttonenv : ButtonEnv | None = None
 
+    def get_action_oracle(self):
+        # If not yet standing on button, move to the closest button.
+        if self.buttonenv.button_status[self_target.button]:
+            actions = self.buttonenv.get_motion_plan()
+        # If standing on button, check if this is a button that the LBM knows about.
+        if current_button in good_lbm_button_set:
+            pressure = query_lbm(current_button)
+        else:
+            pressure = query_human(current_button)
+        return action
 
-class ButtonEnv(gymnasium.Env[ButtonState, ButtonAction]):
-    """Simple 2D grid environment with pressable buttons."""
-
-    render_mode: str = "rgb_array"
-
-    def __init__(self) -> None:
-        self._current_state: ButtonState | None = None
-
-        # Define the observation and action spaces.
-        self.observation_space = FunctionalSpace(
-            contains_fn=lambda s: isinstance(s, ButtonState),
-        )
-        self.action_space = FunctionalSpace(
-            contains_fn=lambda a: isinstance(a, ButtonAction),
-            sample_fn=self._sample_action,
-        )
 
     def step(
-        self, action: ButtonAction
-    ) -> tuple[ButtonState, SupportsFloat, bool, bool, dict[str, Any]]:
+        self, action: LBMPrimitive
+    ) -> tuple[LBMPrimitive, SupportsFloat, bool, bool, dict[str, Any]]:
         assert self._current_state is not None, "Must call reset() first"
         # Handling moving the robot.
         if isinstance(action, MoveButtonAction):
@@ -208,7 +162,7 @@ class ButtonEnv(gymnasium.Env[ButtonState, ButtonAction]):
         *,
         seed: int | None = None,
         options: dict[str, Any] | None = None,
-    ) -> tuple[ButtonState, dict[str, Any]]:
+    ) -> tuple[LBMState, dict[str, Any]]:
 
         # For now, use a constant initial state.
         self._current_state = ButtonState(
@@ -254,39 +208,3 @@ class ButtonEnv(gymnasium.Env[ButtonState, ButtonAction]):
         if pick_which_action == 3:
             return QueryHumanButtonAction()
         return ButtonAction()
-
-    def query_lbm(self) -> PressButtonAction:
-        true_val = self._current_state.button_values[
-            self._current_state.target_button
-        ]
-        if self._current_state.button_good_bad[self._current_state.target_button]:
-            val = np.random.normal(true_val, 0.1)
-        else:
-            val = np.random.normal(true_val, 0.6)
-        return PressButtonAction(val)
-
-    def query_human(self) -> PressButtonAction:
-        good_human = np.random.binomial(0.7)
-        true_val = self._current_state.button_values[
-            self._current_state.target_button
-        ]
-        if good_human:
-            val = np.random.normal(true_val, 0.01)
-        else:
-            val = np.random.normal(true_val, 0.4)
-        return PressButtonAction(val)
-    
-    def get_motion_plan(self, dr, dc) -> list[MoveButtonAction]:
-        start_state = self._current_state.robot_position
-        temp_state = start_state
-        ret = []
-        #lol too lazy to implement good algo
-        for i in range(0, dr - start_state[0], 1):
-            ret.append(MoveButtonAction(1, 0))
-        for i in range(dr - start_state[0], 0, 1):
-            ret.append(MoveButtonAction(-1, 0))
-        for i in range(0, dc - start_state[1], 1):
-            ret.append(MoveButtonAction(0, 1))
-        for j in range(dc - start_state[1], 0, 1):
-            ret.append(MoveButtonAction(0, -1))
-        return ret
